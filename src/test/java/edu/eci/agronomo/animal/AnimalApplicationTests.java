2380bc2f880f0d07d5f7210aa685e36aa8ee4592
Yesid MORA <yesid.mora@mail.escuelaing.edu.co>
2023-03-01 09:08:19 -0500

    structure project

---
+++ src/test/java/edu/eci/agronomo/animal/AnimalApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.agronomo.animal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
